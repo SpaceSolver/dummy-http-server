2afca50085a6641005e2e404c77ce3be09fef71e
ssc-bando <satoshi_bando@solver.co.jp>
2020-03-11 09:27:34 +0900

    change: responce w/ json data { num: "123" }

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	//"encoding/json"
 	"fmt"
 	"net/http"
 )
@@ -10,13 +11,17 @@ const (
 	PortNum string = "8088"
 )
 
+type Ping struct {
+	Status int
+	Result string
+}
+
 func usage() {
 	fmt.Println("for Android Emulator")
 	fmt.Printf("   connect http://10.0.2.2:%s\n", PortNum)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, World")
 	fmt.Printf("<< request details >>\n")
 	fmt.Printf("  length : %v\n", r.ContentLength)
 	fmt.Printf("  method : %v\n", r.Method)
@@ -29,6 +34,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	bufbody.ReadFrom(r.Body)
 	body := bufbody.String()
 	fmt.Printf("%s\n", body)
+
+	// json.Marshal only can parse Public member (its name must start w/ capital charactor)
+
+	//ping := Ping{Status: http.StatusOK, Result: "ok"}
+	//res, err := json.Marshal(ping)
+	//if err != nil {
+	//	http.Error(w, err.Error(), http.StatusInternalServerError)
+	//	return
+	//}
+	//fmt.Printf(">>> server response: [%s]\n", res)
+	//w.Write(res)
+
+	res := "{num:\"123\"}"
+	w.Write([]byte(res))
+	fmt.Printf(">>> server response: [%s]\n", res)
 }
 
 func main() {
